31f3b5fc4449d955e52089dece28144cbc8d5710
ssoso27 <sohi8290@naver.com>
2017-01-08 00:42:00 +0900

    Player_적과의 충돌 구상

--- I'm hero/Assets/Scripts/SH/PlayerManager.cs
+++ I'm hero/Assets/Scripts/SH/PlayerManager.cs
@@ -6,26 +6,30 @@ using UnityEngine;
     Player에 대한 스크립트
 */
 
-public class PlayerManager : MonoBehaviour {
+public partial class PlayerManager : MonoBehaviour {
 
     /*
         public 전역변수는 간편한 테스트를 위함.
         후에 private로 바꾸는 것을 추천
     */
-    
+
     public const float moveSpeedX = 5.0f; // 플레이어 x좌표 이동 속도
     public const float moveSpeedY = 3.0f; // 플레이어 y좌표 이동 속도
     public bool IsLeftRight = false; // 좌우이동 가능 여부
+    public int player_life = 5;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
         PlayerMove(); // 플레이어 이동
-	}
-    
+    }
+}
+
+public partial class PlayerManager { 
+
     // 플레이어 이동
     void PlayerMove()
     {
@@ -48,6 +52,26 @@ public class PlayerManager : MonoBehaviour {
 
         // 이동(할) 거리 만큼 이동
         this.gameObject.transform.Translate(distanceX, distanceY, 0);
+    }
 
+    // 충돌 처리
+    void OnCollisionEnter(Collision col)
+    {
+        EnemyCollision(col); // 적과의 충돌
+    }
+
+    // 적과의 충돌
+    void EnemyCollision(Collision col)
+    {
+        // 만약 충돌체의 Tag가 longpaso 또는 shortpaso면
+        if (col.gameObject.tag == "longpaso" || col.gameObject.tag == "shortpaso")
+        {
+            // 라이프 감소
+            player_life--;
+            // 게임오버 체크 (by. GameManager.cs)
+            // 뒤로 조금 밀려남 or 1초전 위치로 이동
+            // 잠시 무적
+            // 1초간 모든 오브젝트 일시정지
+        }
     }
 }
