bdf4dc322b00e3bc37c571db8a1a19a6b47daea9
ssoso27 <sohi8290@naver.com>
2017-01-07 23:58:13 +0900

    Player_좌우이동 제약

--- I'm hero/Assets/Scripts/SH/PlayerManager.cs
+++ I'm hero/Assets/Scripts/SH/PlayerManager.cs
@@ -15,7 +15,7 @@ public class PlayerManager : MonoBehaviour {
     
     public const float moveSpeedX = 5.0f; // 플레이어 x좌표 이동 속도
     public const float moveSpeedY = 3.0f; // 플레이어 y좌표 이동 속도
-    public bool IsUpDown = false; // 상하이동 가능 여부
+    public bool IsLeftRight = false; // 좌우이동 가능 여부
 
 	// Use this for initialization
 	void Start () {
@@ -25,27 +25,29 @@ public class PlayerManager : MonoBehaviour {
 	void Update () {
         PlayerMove(); // 플레이어 이동
 	}
-
+    
     // 플레이어 이동
     void PlayerMove()
     {
         float distanceX; // x좌표 이동(할) 거리
         float distanceY; // y좌표 이동(할) 거리
-        
-        // 이동(할) 거리 지정
-        distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeedX; // 좌우
-        distanceY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeedY; // 상하
 
-        // 이동(할) 거리 만큼 이동
-        if (IsUpDown)
+        // 이동(할) 거리 지정
+        if (IsLeftRight)
         {
-            // 상하운동 적용
-            this.gameObject.transform.Translate(distanceX, distanceY, 0);
+            // 좌우이동 가능
+            distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeedX; // 좌우
+            distanceY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeedY; // 상하
         }
         else
         {
-            // 상하운동 미적용
-            this.gameObject.transform.Translate(distanceX, 0, 0);
+            // 좌우이동 불가. 자동이동
+            distanceX = Time.deltaTime * moveSpeedX; // 좌우
+            distanceY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeedY; // 상하
         }
+
+        // 이동(할) 거리 만큼 이동
+        this.gameObject.transform.Translate(distanceX, distanceY, 0);
+
     }
 }
