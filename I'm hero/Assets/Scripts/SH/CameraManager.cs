7bd85f15dcf33885786e388a5636412d0d18e569
ssoso27 <sohi8290@naver.com>
2017-01-11 18:51:27 +0900

    카메라 움직임 설정

---
+++ I'm hero/Assets/Scripts/SH/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FollowPlayer();
+	}
+
+    void FollowPlayer()
+    {
+        Vector3 newPosition = new Vector3(GameObject.FindWithTag("Player").transform.position.x, GameObject.FindWithTag("Player").transform.position.y, transform.position.z);
+
+        transform.position = newPosition;
+    }
+}
