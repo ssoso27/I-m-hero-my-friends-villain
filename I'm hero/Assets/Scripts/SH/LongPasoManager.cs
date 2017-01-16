440842a2f0d7c2d22cb1cd1a3b1a710a9d6757af
ssoso27 <sohi8290@naver.com>
2017-01-17 00:45:27 +0900

    Fix Longpaso's rotation

---
+++ I'm hero/Assets/Scripts/SH/LongPasoManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongPasoManager : MonoBehaviour {
+
+    private Vector3 firstPosition;
+
+	// Use this for initialization
+	void Start () {
+        firstPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = firstPosition;
+        transform.rotation = Quaternion.identity;
+	}
+}
