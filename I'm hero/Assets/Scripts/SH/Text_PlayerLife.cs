b70d235aaa62f5d30e5e68ce982e0099aae86f7f
ssoso27 <sohi8290@naver.com>
2017-01-19 13:35:47 +0900

    Print Player's life

---
+++ I'm hero/Assets/Scripts/SH/Text_PlayerLife.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Text_PlayerLife : MonoBehaviour {
+    public Text lifePrint;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ShowLife();
+	}
+
+    void ShowLife()
+    {
+        int playerlife = GameObject.FindWithTag("Player").GetComponent<PlayerManager>().GetPlayerLife();
+        lifePrint.text = "Life : " + playerlife;
+    }
+}
